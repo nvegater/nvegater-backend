fc2cd4bcdba7d772d4cbcf6bd23fe97fccff9e29
P00920345 <nicolas.vegaterrazas@bdr.de>
2020-05-06 22:47:31 +0200

    Added stock model, app, and urls. empty view for now.

--- authSite/authSite/urls.py
+++ authSite/authSite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 urlpatterns = [
+    path("stocks/", include("stocks.urls")),
     path('authapp/', include('authapp.urls')),
     path('admin/', admin.site.urls),
 ]
